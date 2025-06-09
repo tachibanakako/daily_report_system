e45af13ace334b42f267e2c6e39d59e253c6b473
tachibanakako <k-tachibana@fissc.co.jp>
2025-06-09 14:06:25 +0900

    従業員indexアクションの作成

---
+++ src/main/java/actions/EmployeeAction.java
@@ -0,0 +1,48 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+
+import actions.views.EmployeeView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.EmployeeService;
+
+public class EmployeeAction extends ActionBase {
+    private EmployeeService service;
+    
+    //メゾット実行
+    @Override
+    public void process() throws ServletException, IOException{
+        service = new EmployeeService();
+        invoke();
+        service.close();
+    }
+    
+    //一覧画面を表示
+    public void index() throws ServletException, IOException{
+        //データ取得
+        int page = getPage();
+        List<EmployeeView> employees = service.getPerPage(page);
+        //データ件数取得
+        long employeeCount = service.countAll();
+        
+        putRequestScope(AttributeConst.EMPLOYEES, employees);
+        putRequestScope(AttributeConst.EMP_COUNT, employeeCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+        
+        //セッションにフラッシュメッセージが設定される場合、リクエストスコープに移し、セッションからは削除
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if(flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+        //一覧画面表示
+        forward(ForwardConst.FW_EMP_INDEX);
+    }
+
+}
